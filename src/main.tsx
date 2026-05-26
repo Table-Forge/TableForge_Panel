61efc1efcd844c4ea6e130c52b87e3098320f256
ferrazmanu <ferraz.manuela@hotmail.com>
2026-05-25 21:03:40 -0300

    chore(devtools): add react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { handleError } from "@/src/utils/error-handler";
@@ -28,6 +29,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 );
