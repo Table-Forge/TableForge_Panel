4ec6b3b415f05d7bf3f009c698a60bf4fb2b1d16
ferrazmanu <ferraz.manuela@hotmail.com>
2026-09-21 20:25:53 -0300

    chore(config): update S3 bucket URLs to new aws account

--- src/constants/logos.ts
+++ src/constants/logos.ts
@@ -2,14 +2,14 @@ import { useBoundStore } from "@/src/store";
 
 export const BRAND_LOGOS = {
   dark: {
-    vertical: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/394a0616-6467-4be9-b6ad-6df1a5a57cc9.webp?v=1",
-    minimal: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/d4037d09-0893-4098-bc52-f94291c91c00.png",
-    horizontal: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/0b85dfdf-3c07-4aad-b8fe-0c88e2bbfa3f.webp?v=1",
+    vertical: "https://table-forge.s3.amazonaws.com/development/public/images/394a0616-6467-4be9-b6ad-6df1a5a57cc9.webp?v=1",
+    minimal: "https://table-forge.s3.amazonaws.com/development/public/images/d4037d09-0893-4098-bc52-f94291c91c00.png",
+    horizontal: "https://table-forge.s3.amazonaws.com/development/public/images/0b85dfdf-3c07-4aad-b8fe-0c88e2bbfa3f.webp?v=1",
   },
   light: {
-    vertical: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/6146fade-9fde-4082-b17c-2643d7ddedaf.png",
-    minimal: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/0a9406bb-e930-4238-9a21-59b5bb0eb80b.png",
-    horizontal: "https://tableforge-bucket.s3.amazonaws.com/development/public/images/221ee0bd-1c93-4b77-a0a5-aef9d911c02c.png",
+    vertical: "https://table-forge.s3.amazonaws.com/development/public/images/6146fade-9fde-4082-b17c-2643d7ddedaf.png",
+    minimal: "https://table-forge.s3.amazonaws.com/development/public/images/0a9406bb-e930-4238-9a21-59b5bb0eb80b.png",
+    horizontal: "https://table-forge.s3.amazonaws.com/development/public/images/221ee0bd-1c93-4b77-a0a5-aef9d911c02c.png",
   },
 } as const;
 
